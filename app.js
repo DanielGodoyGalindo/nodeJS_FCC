aa97e654cd9d0416a38520fd30757d53f580b161
Daniel Godoy <danielgodoy66@gmail.com>
2025-01-01 21:57:05 +0100

    modulo fs

--- app.js
+++ app.js
@@ -1,32 +1,72 @@
-// Módulo timers
-// Contiene funciones que ejecutan código despues de un cierto periodo
-// de tiempo (podemos simular eventos asíncronos)
+// Módulo fs (filesystem)
+// modulo que contiene funcionalidad util para trabajar con el sistema
+// de archivos (leer, modificar, copiar, eliminar, renombrar)
+// Todos los metodos son asincronos por defecto
+// Se pueden escoger versiones sincronas de estos métodos agregando "sync"
+
+import { appendFile, readFile, rename, unlink, writeFile } from 'node:fs';
 
 
+//////////////// Metodos asincronos (por defecto) //////////////////////
+// lectura de fichero index.html
+/* readFile('./index.html', 'utf-8', (error, data) => {
+  error ? console.log(error) : console.log(data);
+}); */
 
-function mostrarTema(tema) {
-  console.log(`Estoy aprendiendo ${tema}`);
-}
+// renombrar un fichero
+/* rename('index.html', 'main.html', (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log('Nombre de fichero modificado correctamente!');
+}) */
 
-const sumar = (a, b) => {
-  console.log(a + b);
-}
+// Agregar contenido al final de un archivo
+/* appendFile('index.html', '<p>Hola</p>', (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log('Fichero actualizado correctamente!');
+}) */
 
-//////// setTimeout() --> ejecutar código despues de un número de milisegundos
-// setTimeout(funcion,milisegundos,[argumentos])
-// setTimeout(mostrarTema, 3000, 'Node.js');
-// setTimeout(sumar, 2000, 5, 6);
+// Reemplazar todo el contenido del archivo
+/* writeFile('index.html', 'Contenido nuevo', (error) => {
+  if (!error) {
+    console.log('Contenido reemplazado con éxito');
+  } else {
+    throw error;
+  }
+}) */
 
-/////////// setImmediate() -->
-// Se usa para ejecutar código asíncrono en la próxima
-// iteración del ciclo de eventos (lo más pronto posible)
-// setImmediate(funcion,argumento1,argumento2)
-// se ejecuta despues del código síncrono
-/* console.log('Antes de setImmediate()');
-setImmediate(mostrarTema, 'Node.js'); --> se ejecuta el último
-console.log('Después de setImmediate()'); */
+// Eliminar fichero
+/* unlink('fichero.txt', (error) => {
+  if (error)
+    throw error;
+  console.log('Fichero eliminado con éxito!');
+}) */
 
-//////// setInterval() --> ejecutar un códgio un numero infinito de veces
-// setInterval(funcion,intervalo,[arg1,arg2,...])
-// setInterval(mostrarTema,2000,'Node.js');
-// setInterval(sumar, 1500, 3, 4);
+/////////////////// Metodos sincronos (añadiendo 'sync') /////////////////////
+/* // lectura de fichero index.html
+console.log('Antes de leer el fichero.');
+console.log(readFileSync('index.html', 'utf-8'));
+console.log('Despues de leer el fichero.');
+
+// renombrar un fichero
+console.log('Antes de renombrar el fichero.');
+renameSync('index.html', 'main.html');
+console.log('Despues de renombrar el fichero.');
+
+// Agregar contenido al final de un archivo
+console.log('Antes de agregar contenido en el fichero.');
+appendFileSync('index.html', '<p>Hola</p>');
+console.log('Despues de agregar contenido en el fichero.');
+
+// Reemplazar todo el contenido del archivo
+console.log('Antes de reemplazar todo el contenido en el fichero.');
+writeFileSync('index.html', 'Contenido nuevo');
+console.log('Despues de agregar contenido en el fichero.');
+
+// Eliminar fichero
+console.log('Antes de eliminar el fichero.');
+unlinkSync('fichero.txt');
+console.log('Despues de eliminar el fichero.'); */
