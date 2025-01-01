8872b38ebd644261484d846a74dc9ba86e75d671
Daniel Godoy <danielgodoy66@gmail.com>
2025-01-01 20:58:33 +0100

    modulo timers

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-// Modulo os
-// obtener funcionalidad del sistema operativo usado con nodejs
+// Módulo timers
+// Contiene funciones que ejecutan código despues de un cierto periodo
+// de tiempo (podemos simular eventos asíncronos)
 
-// importacion 
-// const os = require('node:os');
-import os from 'node:os';
 
-// Indica cual es el SO del equipo
-console.log(os.type());
 
-// Directorio principal del usuario
-console.log(os.homedir());
+function mostrarTema(tema) {
+  console.log(`Estoy aprendiendo ${tema}`);
+}
 
-// Numero de segundos transcurridos desde que se inició el equipo
-console.log(os.uptime());
+const sumar = (a, b) => {
+  console.log(a + b);
+}
 
-// Información del usuario
-console.log(os.userInfo());
+//////// setTimeout() --> ejecutar código despues de un número de milisegundos
+// setTimeout(funcion,milisegundos,[argumentos])
+// setTimeout(mostrarTema, 3000, 'Node.js');
+// setTimeout(sumar, 2000, 5, 6);
+
+/////////// setImmediate() -->
+// Se usa para ejecutar código asíncrono en la próxima
+// iteración del ciclo de eventos (lo más pronto posible)
+// setImmediate(funcion,argumento1,argumento2)
+// se ejecuta despues del código síncrono
+/* console.log('Antes de setImmediate()');
+setImmediate(mostrarTema, 'Node.js'); --> se ejecuta el último
+console.log('Después de setImmediate()'); */
+
+//////// setInterval() --> ejecutar un códgio un numero infinito de veces
+// setInterval(funcion,intervalo,[arg1,arg2,...])
+// setInterval(mostrarTema,2000,'Node.js');
+// setInterval(sumar, 1500, 3, 4);
