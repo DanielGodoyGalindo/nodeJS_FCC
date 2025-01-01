6b22eb3d1f6c6c64b4edb932bd9255846ccb9ef4
Daniel Godoy <danielgodoy66@gmail.com>
2025-01-01 18:28:05 +0100

    modulo process

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-// Módulo console
-console.log('Hola Mundo');
-console.warn('Ha ocurrido un error...');
+// Modulo process
+// The process object provides information about, and control over, the current Node.js process.
 
-// crear un objeto de error
-console.error(new Error('Ocurrió un error!'));
+// mostrar información del objeto process
+// console.log(process);
+
+// Environment
+// console.log(process.env);
+
+// Muestra cuales son los argumentos de ejecutar el fichero app.js
+// el primero es "node" y el segundo el fichero "app.js"
+// console.log(process.argv);
+
+// Uso de memoria
+// console.log(process.memoryUsage());
